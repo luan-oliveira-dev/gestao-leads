c130d1b6c66d310dc64db198f169a13f80bdb6cc
Luan Oliveira <luannsdo@gmail.com>
2026-02-12 19:41:58 -0300

    feat: criando as migrations de leads [status_leads|leads|lead_stage]

---
+++ migrations/2026_02_12_223256_create_leads_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('telephone');
+
+            $table->unsignedBigInteger('status_lead_id');
+            $table->foreign('status_lead_id')
+            ->references('id')
+            ->on('status_leads')
+            ->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leads');
+    }
+};
