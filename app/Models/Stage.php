53eb83a205ec9081176182b9ff7661470a9c74dc
Luan Oliveira <luannsdo@gmail.com>
2026-02-13 14:20:39 -0300

    feat: criando as models relacionadas ao Stage

---
+++ app/Models/Stage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Stage extends Model
+{
+    use HasFactory;
+
+    use SoftDeletes;
+
+    protected $table = 'stages';
+
+    protected $fillable = [
+        'name',
+        'position',
+        'funnel_id',
+        'status_stage_id',
+    ];
+}
