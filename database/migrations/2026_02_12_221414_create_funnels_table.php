292c373e8fe9b562a7868b3ab1ccbb7345dfc612
Luan Oliveira <luannsdo@gmail.com>
2026-02-13 09:18:58 -0300

    adicionando arquivos pendentes

---
+++ database/migrations/2026_02_12_221414_create_funnels_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('funnels', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onUpdate('cascade');
+
+            $table->unsignedBigInteger('manager_id');
+            $table->foreign('manager_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade');
+
+            $table->unsignedBigInteger('status_funnel_id');
+            $table->foreign('status_funnel_id')
+                ->references('id')
+                ->on('status_funnels')
+                ->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('funnels');
+    }
+};
